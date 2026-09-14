9eaa6b5104e6d6cd5d3869d43f0da0ac00f1ea07
Himanshu <himanshu1988mishra@gmail.com>
2026-09-14 23:12:16 +0530

    createElelement.js | added ;earning notes/comments

--- createElement.js
+++ createElement.js
@@ -16,3 +16,9 @@ const parentDiv = React.createElement("div", { id: "parent" }, childDiv);
 const domRoot = document.getElementById("root");
 const reactRoot = ReactDOM.createRoot(domRoot);
 reactRoot.render(parentDiv);
+
+/*
+ * This is a simple React application that creates a hierarchy of elements and renders them to the DOM.
+ * heading, heading2 etc. are react elements not HTML elements, those are actually objects.
+ * when ReactDOM renders reactRoot then ReactDOM creates HTML elements from those react elements and adds them to the DOM.
+ */
